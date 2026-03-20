d9c5e174e3e4f8b56f8d5f158fe24a6ad6606a48
juliajohanson <juliajohansonpeniche@gmail.com>
2026-03-20 11:48:04 -0300

    foi adicionado ; no final da 4 linha

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(){
     printf("Eduardo Barcelos De Carvalho Braziliano")
-    printf("Julia Johanson Peniche Dias Da Silva")
+    printf("Julia Johanson Peniche Dias Da Silva");
 }
