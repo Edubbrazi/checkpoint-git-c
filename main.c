d7c4290f6c4a9e7c117c333afa4364f3c7b86c6b
edubbrazi@gmail.com <edubbrazi@gmail.com>
2026-03-20 11:52:09 -0300

    foi adicionado o ; no final da linha 4 para terminar a linha e o return 0 na linha 6

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main(){
-    printf("Eduardo Barcelos De Carvalho Braziliano")
+
+    printf("Eduardo Barcelos De Carvalho Braziliano");
     printf("Julia Johanson Peniche Dias Da Silva");
+    return 0
 }
